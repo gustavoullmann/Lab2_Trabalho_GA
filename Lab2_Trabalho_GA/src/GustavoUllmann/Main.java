57d9a3ad5f775910117799debc941312a9197af6
gustavo <gustavo@gustavo-Vostro-3460>
2020-09-20 21:13:10 -0300

    criada a classe menu

--- Lab2_Trabalho_GA/src/GustavoUllmann/Main.java
+++ Lab2_Trabalho_GA/src/GustavoUllmann/Main.java
@@ -1,11 +1,25 @@
 package GustavoUllmann;
 
+import java.awt.Desktop;
+import java.net.URI;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("teste");
-
+		Menu.menu();
+		
 	}
-
+		
+		
 }
+
+/*
+ * System.out.println(Desktop.isDesktopSupported());
+		
+		try{
+            URI link = new URI("www.google.com");
+            Desktop.getDesktop().browse(link);
+        }catch(Exception erro){
+            System.out.println(erro);
+        }	*/
